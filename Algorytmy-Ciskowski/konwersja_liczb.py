ce0e5e03046ef8b61aef032bbb34d723c78a2e5e
hubertusol <hubertusol@op.pl>
2024-01-30 23:10:42 +0100

    algorytmy2

--- Algorytmy-Ciskowski/konwersja_liczb.py
+++ Algorytmy-Ciskowski/konwersja_liczb.py
@@ -1,4 +1,5 @@
 #ĆW.01. Zadanie 4
+#Hubert Sołtys
 try:
     x = int(input("Proszę wpisać liczbę dziesiętną do konwersji: "))
     y = int(input("Proszę wpisać podstawę systemu liczbowego (2-36): "))
@@ -16,7 +17,7 @@ def zamien(zmienna, podstawa):
         if reszta in range(0,10):
             wynik.insert(0,reszta) 
         else:
-            wynik.insert(0,chr(55+reszta)) #konwersja liczby na ASCII, gdy liczba jest większa od 9, (począwszy od A)
+            wynik.insert(0,chr(55+reszta))
         zamien(zmienna,podstawa)
 zamien(x,y)
-for i in wynik: print(i,end='') #wyświetlenie listy w postaci stringa
+for i in wynik: print(i,end='')
---
+++ Algorytmy-Ciskowski/rzymskie_liczby.py
@@ -0,0 +1,20 @@
+#ĆW.01. Zadanie 3
+#Hubert Sołtys
+#W poleceniu jest napisane, że trzeba poprawić błąd w algorytmie
+#Moim zdaniem błędem jest użycie instrukcji switch
+#Poniżej wykorzystałem szybszy i prostszy sposób
+try:
+    x = int(input("Proszę wpisać liczbę do zamiany na rzymską: "))
+    if x<1:
+        print("Liczba musi być większa od 0")
+        exit()
+except ValueError:
+    print("Prosze wpisac liczbe calkowita")
+    exit()
+arabska=x
+liczby_rzymskie = {'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+print(f"Liczba {x} zamieniona na zapis rzymski to: ",end='')
+for rzymska,wartosc in liczby_rzymskie.items():
+    while arabska>=wartosc:
+        arabska-=wartosc
+        print(rzymska,end='')
