d4c9956c6cb24b7db393c8ce23cb685adb137237
hubertusol <hubertusol@op.pl>
2024-03-18 15:42:28 +0100

    tiger

--- OB-Python-Lubicka/18.03/zadania prawdziwe/tiger.py
+++ OB-Python-Lubicka/18.03/zadania prawdziwe/tiger.py
@@ -1,4 +1,17 @@
 class Cat:
-    legs=4
-    tail=1
-    def __init__(self)
+    def __init__(self,masa,dlugosc_zycia,predkosc):
+        self.masa=masa
+        self.dlugosc_zycia=dlugosc_zycia
+        self.predkosc=predkosc
+    def __str__(self):
+        return f"Kot waży {self.masa}kg, średnio żyje {self.dlugosc_zycia} lat, może biec z maksymalną prędkością {self.predkosc} km/h"
+    class Tiger:
+        def __init__(self,masa,dlugosc_zycia,predkosc, umaszczenie):
+            Cat.__init__(self,masa,dlugosc_zycia,predkosc)
+            self.umaszczenie=umaszczenie
+        def __str__(self):
+            return f"Tygrys waży {self.masa}kg, średnio żyje {self.dlugosc_zycia} lat, może biec z maksymalną prędkością {self.predkosc} km/h oraz ma {self.umaszczenie} umaszczenie"
+dachowiec=Cat(5,14,20)
+print(dachowiec)
+tygrys=Cat.Tiger(310,26,65,"pręgowate")
+print(tygrys)
